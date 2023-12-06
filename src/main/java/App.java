16e3e785836efcdc2f696eca9a5ee08c4e9a53b7
VitaliyGerasimov <gerasimov-1996@inbox.ru>
2023-12-06 10:45:29 +0500

    First change

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,14 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat =
+                (Cat) applicationContext.getBean("cat");
+        Cat cat2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println("Ссылки 'HelloWorld' равны? " + (bean == bean2));
+        System.out.println("Ссылки 'Cat' равны? " + (cat == cat2));
         System.out.println(bean.getMessage());
     }
 }
